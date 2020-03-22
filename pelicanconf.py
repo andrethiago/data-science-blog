fae094e35e017ca3edb1f751bfcc264571f79f7f
Andre Thiago Souza da Silva <andrethiago@mpf.mp.br>
2020-03-22 16:55:35 -0300

    Configuração do tema Flex.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,21 @@ from __future__ import unicode_literals
 
 AUTHOR = 'André Thiago'
 SITENAME = 'Robô Aprendiz'
-SITEURL = ''
+SITETITLE = 'Robô Aprendiz'
+SITESUBTITLE = 'Aprendizagem de máquina com técnicas de IA'
+SITEDESCRIPTION = SITETITLE + ' - ' + SITESUBTITLE
+SITEURL = 'http://localhost:8000'
+SITELOGO = SITEURL + '/images/artificial-intelligence.png'
 
 PATH = 'content'
 
 TIMEZONE = 'America/Sao_Paulo'
 
-DEFAULT_LANG = 'pt'
+#DEFAULT_LANG = 'pt'
+MAIN_MENU = True
+LINKS_IN_NEW_TAB = 'internal'
+USE_FOLDER_AS_CATEGORY = False
+HOME_HIDE_TAGS = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,14 +28,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Sobre', '#'),
+         ('Contato', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
+
+MENUITEMS = (('Arquivos', '/archives.html'),
+             ('Categorias', '/categories.html'),
+             ('Tags', '/tags.html'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -35,12 +45,12 @@ DEFAULT_PAGINATION = 10
 #RELATIVE_URLS = True
 
 # Theme
-THEME = 'themes/pelican-blueidea'
+THEME = 'themes/Flex' #'themes/clean-blog'
 
 # Path to the folder containing the plugins
 PLUGIN_PATHS = ['pelican-plugins']
 # Enabled plugins
-PLUGINS = ['sitemap', 'pelican-ipynb.markup']
+PLUGINS = ['sitemap', 'pelican-ipynb.markup', 'i18n_subsites']
 
 # sitemap plugin configuration
 SITEMAP = {
@@ -60,3 +70,16 @@ SITEMAP = {
 # jupyter plugin configuration
 MARKUP = ('md', 'ipynb')
 IGNORE_FILES = [".ipynb_checkpoints"]
+IPYNB_SKIP_CSS=True
+
+# Enable Jinja2 i18n extension used to parse translations.
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+
+# Translate to German.
+DEFAULT_LANG = 'pt_BR'
+#OG_LOCALE = 'pt_BR'
+#LOCALE = 'pt_BR'
+
+# Default theme language.
+I18N_TEMPLATES_LANG = 'pt_BR'
